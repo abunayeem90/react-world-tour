19667036d8c7b30ed4c1b5b776400a470f985f46
abunayeem90 <naeemahmmed4@gmail.com>
2024-03-11 12:57:58 +0600

    first commit

---
+++ src/Component/Country/Country.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import './Country.css';
+
+const Country = ({country, handleVisitedCountry }) => {
+    // console.log(country);
+    const {name, flags, population, area} = country;
+    
+    const [visited, setVisited]= useState(false);
+    
+    const handleVisited = () =>{
+        // setVisited(true);
+        setVisited(!visited);
+    }
+
+    const passWithParams = () => handleVisitedCountry(country);
+
+    // console.log(handleVisitedCountry);
+
+    return (
+        <div className={`country ${visited && 'visited'}`}>
+        {/* <div className={`country ${visited ? 'visited' : 'non-visited'}`}> */}
+            <h2 style={{color: visited? 'purple' : 'white'}}>{name.common}</h2>
+            <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+           <div className='btn'>
+           <button onClick={passWithParams}>Mark Visited</button>
+            {/* <button onClick={handleVisited}>Visited</button> */}
+            <button onClick={handleVisited}> {visited ? 'Visited':'Going'}</button>
+            {/* {visited && 'I have Visited this Country.'} */}
+            {visited ? 'I have Visited this Country.' : 'I want to visite this country'}
+           </div>
+        </div>
+    );
+};
+
+export default Country;
